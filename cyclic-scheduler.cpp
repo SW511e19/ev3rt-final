92a0ab6404ddab8b3de422b4243abde9d530016b
sembrik <peter@servon.eu>
2019-11-30 10:59:30 +0000

    Essentials

--- cyclic-scheduler.cpp
+++ cyclic-scheduler.cpp
@@ -3,12 +3,47 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 #include <iostream>
+#define ever (;;)
 
 using std::cout;
 using std::endl;
 
+void runMD() {
+    cout << "Running Multiple Card Dispenser Task" << endl;
+}
+
+void runSD() {
+    cout << "Running Singular Card Dispenser Task" << endl;
+}
+
+bool checkCardPlaced() {
+    cout << "Checking if Card is Placed" << endl;
+    return false;
+}
+
+int getCardPlacement() {
+    cout << "Getting card Placement from PI" << endl;
+    return 1;
+}
+
+void pushPiston(){
+    cout << "Pushed the Piston" << endl;
+}
+
+void positionCardCollector(int placement) {
+    cout << "Position the Card Collector..." << endl;
+    pushPiston();
+}
+
+
 int main()
 {
-    cout << "Hello, World! test" << endl;
+    for ever {
+        runMD();
+        runSD();
+        if (checkCardPlaced()){
+            positionCardCollector(getCardPlacement());
+        }
+    }
     return 0;
 }
